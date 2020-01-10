482522a58c26a8c8110b50823feb26b2bebadcac
李福元 <lifuyuan@swifthealth.cn>
2020-01-10 17:36:02 +0800

    updte

--- python/first/data_type.py
+++ python/first/data_type.py
@@ -22,6 +22,7 @@
               ④Python3 的六个标准数据类型中：
                 不可变数据（3 个）：Number（数字）、String（字符串）、Tuple（元组）；
                 可变数据（3 个）：List（列表）、Dictionary（字典）、Set（集合）。
+              注意 : string,list,tuple都属于sequence(序列).
 """
 
 '''
