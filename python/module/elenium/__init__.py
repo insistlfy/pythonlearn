cf84fd2f0dc7e1a8f842a867d3c4d0602d9e2d94
LFY <lifuyuan@swifthealth.cn>
2020-02-07 20:42:24 +0800

    add elenium

---
+++ python/module/elenium/__init__.py
@@ -0,0 +1,8 @@
+# !usr/bin/env python
+# -*- coding:utf-8 _*-
+"""
+@author:lfy
+@file: __init__.py.py
+@time: 2020-02-07 下午8:40
+@description: __init__.py
+"""
---
+++ python/module/elenium/elenium_learn_01.py
@@ -0,0 +1,8 @@
+# !usr/bin/env python
+# -*- coding:utf-8 _*-
+"""
+@author:lfy
+@file: elenium_learn_01.py
+@time: 2020-02-07 下午8:41
+@description: elenium_learn01
+"""
